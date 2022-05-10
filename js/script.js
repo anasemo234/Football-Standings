2b1abfa8d04bd8203ab870cbe14f16c3369dddfa
Anasemos Kassahun <Anasemosaka@gmail.com>
2022-05-10 11:47:42 -0400

    event listener added for button

--- js/script.js
+++ js/script.js
@@ -3,21 +3,24 @@
 const URL = 'https://api.kanye.rest/'
 
 // Elements
-const $quote = $('#quote')
-const $para = $('#para')
-
+const $quote = $('#quote');
+const $para = $('#para');
+const $btn = $('#getQuoteBtn');
 // Event listeners
-
+$btn.on('click', handleGetData)  // when button is clicked data will be ran 
 
 
 
 // Functions
 
-function handleGetData() {
+function handleGetData(event) {
+
+    event.preventDefault()      // prevent refreshing the whole page
 
     $.ajax(URL).then(function(data) {
    console.log(data) 
-   $quote.text(data.quote)      // gets data when called
+   $para.text(data.quote)
+//    $('#content').append(``)      // gets data when called
     })
 
 
