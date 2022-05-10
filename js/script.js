d529fd0b19d99fca1a6b8247890870bc3e94e5e1
Anasemos Kassahun <Anasemosaka@gmail.com>
2022-05-10 11:14:07 -0400

    added styling and updated variables

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
+
+// Variables
 const URL = 'https://api.kanye.rest/'
+
+// Elements
+const $quote = $('#quote')
+const $para = $('#para')
+
+// Event listeners
+
+
+
+
+// Functions
+
 function handleGetData() {
 
     $.ajax(URL).then(function(data) {
-    console.log(data)
+   console.log(data) 
+   $quote.text(data.quote)      // gets data when called
     })
 
 
-    $getQuoteBtn.on('click', getQuote);
+    
 } 
